4ffbfe6ecbb288912322fd1d8ef69360b17583df
Seanti <seanpizzaspark@gmail.com>
2024-07-12 12:09:22 +0800

    chore: Update Savemore page to fetch product data from API

--- dashboard/src/pages/Savemore.jsx
+++ dashboard/src/pages/Savemore.jsx
@@ -1,10 +1,37 @@
-import { React, useState, useEffect } from "react";
+import React, { useState, useEffect } from "react";
 import { useNavigate } from "react-router-dom";
 import axios from "axios";
 import Sidebar from "../components/common/Sidebar";
+import ProductsContainer from "../components/common/ProductsContainer";
+import OrderSummary from "../components/OrderSummary";
+import { Box } from "@mui/material";
+import OrderConfirmation from "../components/OrderConfirmation";
+import SuccessDialog from "../components/SuccessDialog";
 
 export default function Savemore() {
     const navigate = useNavigate();
+    const { VITE_REACT_APP_API_HOST, VITE_REACT_APP_UNIONBANK_TOKEN } =
+        import.meta.env;
+    const [openConfirmation, setOpenConfirmation] = useState(false);
+    const [dataList, setDataList] = useState([]);
+    const [successDialogOpen, setSuccessDialogOpen] = useState(false);
+    const [referenceId, setReferenceId] = useState('');
+
+    useEffect(() => {
+        axios
+            .get(`http://192.168.10.25:3004/getallproducts`)
+            .then((response) => {
+                const initializedData = response.data.map((item) => ({
+                    ...item,
+                    quantity: 0,
+                }));
+                setDataList(initializedData);
+            })
+            .catch((error) => {
+                console.error("Error fetching dataList:", error);
+                setDataList([]);
+            });
+    }, []);
 
     return (
         <div className="page">
